3829336b4dfc493ff5bcf1d599662b0f4bb6652b
claudettenyu-source <claudettenyu@gmail.com>
2025-10-09 20:47:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,96 +1,106 @@
 #include <stdio.h>
-
 #include <stdlib.h>
+#include <stdint.h>
 
-typedef struct
-{
-    int hauteur;
-    int largeur;
-} Image;
-typedef struct 
-{
-    int r;
-    int g;
-    int b;
-}Pixel;
+// --- 1. Structures BMP ---
 
+// Utiliser #pragma pack(push, 1) pour éviter le padding (rembourrage) des structures.
+#pragma pack(push, 1)
 
-void creer_image()
-{
-    Image img;
-    Pixel couleur;
-    printf("entrer la largeur de l'image : ");
-    scanf("%d", &img.largeur);
-    getchar();
-    printf("entrer la hauteur de l'image : ");
-    scanf("%d", &img.hauteur);
+typedef struct {
+    uint16_t bfType;      // Signature 'BM' (0x4D42)
+    uint32_t bfSize;      // Taille totale du fichier
+    uint16_t bfReserved1;
+    uint16_t bfReserved2;
+    uint32_t bfOffBits;   // Offset vers les données de pixels
+} BMPFileHeader;
 
-    FILE *fichier = fopen("image.ppm", "w");
-    if (fichier == NULL)
-    {
-        printf("Erreur lors de la création du fichier.\n");
-        return;
+typedef struct {
+    uint32_t biSize;          // Taille de ce header (40)
+    int32_t  biWidth;         // Largeur
+    int32_t  biHeight;        // Hauteur
+    uint16_t biPlanes;
+    uint16_t biBitCount;      // Bits par pixel (doit être 24 ici)
+    uint32_t biCompression;   // Doit être 0
+    uint32_t biSizeImage;
+    int32_t  biXPelsPerMeter;
+    int32_t  biYPelsPerMeter;
+    uint32_t biClrUsed;
+    uint32_t biClrImportant;
+} BMPInfoHeader;
+
+// Pixel stocké en BGR (Bleu, Vert, Rouge) en 24 bits
+typedef struct {
+    uint8_t blue;
+    uint8_t green;
+    uint8_t red;
+} Pixel;
+
+#pragma pack(pop)
+
+// --- 2. Fonction de Filtre ---
+
+/**
+ * Applique le filtre négatif à l'image.
+ * Pour chaque composante de couleur (R, G, B), calcule 255 - valeur_actuelle.
+ */
+void applyNegativeFilter(Pixel* pixels, int width, int height) {
+    if (!pixels) return;
+
+    long total_pixels = (long)width * height;
+
+    for (long i = 0; i < total_pixels; ++i) {
+        // Négatif pour la composante Bleue
+        pixels[i].blue = 255 - pixels[i].blue;
+        
+        // Négatif pour la composante Verte
+        pixels[i].green = 255 - pixels[i].green;
+        
+        // Négatif pour la composante Rouge
+        pixels[i].red = 255 - pixels[i].red;
     }
-    fprintf(fichier, "P3\n%d %d\n255\n", img.largeur, img.hauteur);
-
-    for (int i = img.hauteur - 1; i >= 0; i--)
-    {
-        for (int j = 0; j < img.largeur; j++)
-        {
-            float r = (float)j / (float)img.hauteur;
-            float g = (float)i / (float)img.largeur;
-            float b = 0.2;
-            int ir = (int)(r * 255.99);
-            int ig = (int)(g * 255.99);
-            int ib = (int)(b * 255.99);
-
-            couleur.r = ir;
-            couleur.g = ig;
-            couleur.b = ib;
-
-            fprintf(fichier, "%d %d %d ", ir, ig, ib);
-        }
-        if(i > 0)
-            fprintf(fichier, "\n");
-    }
-    fclose(fichier); 
-    printf("Image générée et enregistrée dans 'image.ppm'\n");
 }
 
-void Filtre_Median(int tab[], int n){
+// --- 3. Fonctions Utilitaire (Lecture/Écriture) ---
 
-}
+Pixel* loadImageBMP(const char* filename, BMPFileHeader* fileHeader, BMPInfoHeader* infoHeader);
+void saveImageBMP(const char* filename, const BMPFileHeader* fileHeader, const BMPInfoHeader* infoHeader, const Pixel* pixels);
 
-int main()
-{
-    int choix;
-    do
-    {
-        printf("--Bienvenue dans la manipulation des images PPM--\n");
-        printf("1.Afficher l'image utilisee\n");
-        printf("2.Eclaircir les pixels\n");
-        printf("3.Passer en noir et blanc \n");
-        printf("4.Le negatif de l'image\n");
-        printf("5.Afficher la taille de l'image\n");
-        printf("6.Decouper et Afficher une partie de l'image\n");
-        printf("7.filtre median\n");
-        printf("0.Quitter\n");
-        printf("Entrer votre choix : ");
-        scanf("%d", &choix);
+// NOTE: Le corps des fonctions loadImageBMP et saveImageBMP est omis ici pour la concision, 
+// mais il doit être le même que dans la réponse précédente pour gérer les headers et le padding.
 
-        switch (choix)
-        {
-        case 1:
-            printf("voici l'image utiliser\n");
-            creer_image();
-            break;
-        case 0:
-            printf("Aurevoir\n");
-            break;
-        default:
-            printf("choix indisponible\n");
-            break;
-        }
-    } while (choix != 0);
+// --- 4. Fonction Principale (main) ---
+int main() {
+    const char* INPUT_FILE = "input.bmp";
+    const char* OUTPUT_FILE = "output_negative.bmp";
+
+    BMPFileHeader fileHeader;
+    BMPInfoHeader infoHeader;
+    Pixel* pixels = NULL;
+
+    printf("--- Programme de création de négatif d'image ---\n");
+    
+    // 1. Chargement de l'image (Assurez-vous que cette fonction est bien définie)
+    pixels = loadImageBMP(INPUT_FILE, &fileHeader, &infoHeader);
+
+    if (pixels == NULL) {
+        fprintf(stderr, "Échec du chargement de l'image ou format non supporté.\n");
+        return 1;
+    }
+
+    printf("Image chargée : %d x %d pixels.\n", infoHeader.biWidth, infoHeader.biHeight);
+
+    // 2. Application du filtre
+    printf("Application du filtre négatif...\n");
+    applyNegativeFilter(pixels, infoHeader.biWidth, infoHeader.biHeight);
+
+    // 3. Sauvegarde de l'image
+    saveImageBMP(OUTPUT_FILE, &fileHeader, &infoHeader, pixels);
+
+    printf("Image négative enregistrée sous '%s'.\n", OUTPUT_FILE);
+    
+    // 4. Nettoyage de la mémoire
+    free(pixels); 
+    
     return 0;
 }
