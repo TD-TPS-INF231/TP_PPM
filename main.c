088b22c0275e2a591031a1f935909159b53da561
divrogbelkann <belkannemmanueld@gmail.com>
2025-10-10 12:58:49 +0100

    Update main.c

--- main.c
+++ main.c
@@ -219,11 +219,11 @@ int main()
             printf("voici l'image utiliser\n");
             creer_image();
             break;
-            case 2;
+            case 2:
             printf("voici le négatif de l'image\n");
             creer_negatif_image();
             break;
-            case 3;
+            case 3:
             printf("voici votre image découper");
             decouper_partie_image();
             break;
